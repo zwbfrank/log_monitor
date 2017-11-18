53debf0d0299fb9a1b3addc80b8e7d9f37938e68
frank <874032981@qq.com>
2017-11-18 20:05:11 +0800

    html update

--- log_analyze/views.py
+++ log_analyze/views.py
@@ -5,10 +5,89 @@ from .models import BizServiceError
 # Create your views here.
 def index(request):
 
-	return render(request,'log_analyze/index.html')
+    return render(request,'log_analyze/index.html')
 
 def biz_service_error(request):
-	logs = BizServiceError.objects.all()
-	logs = reversed(logs)
-	context = {'logs':logs}
-	return render(request,'log_analyze/bizerror.html',context)
+    logs = BizServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/bizerror.html',context)
+
+def alibblife_web_error(request):
+    logs = AlibblifeWebError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/alibblifeerror.html',context)
+
+def admin_web_error(request):
+    logs = AdminWebError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/adminerror.html',context)
+
+def alilife_service_error(request):
+    logs = BizServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/alilifeerror.html',context)
+
+def commun_service_error(request):
+    logs = CommunicationServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/communerror.html',context)
+
+def config_service_error(request):
+    logs = ConfigServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/configerror.html',context)
+
+def mobile_web_error(request):
+    logs = MobileWebError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/mobileerror.html',context)
+
+def order_service_error(request):
+    logs = OrderServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/ordererror.html',context)
+
+def pay_service_error(request):
+    logs = PayServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/payerror.html',context)
+
+def promotion_service_error(request):
+    logs = PromotionServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/promotionerror.html',context)
+
+def shop_service_error(request):
+    logs = ShopServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/shoperror.html',context)
+
+def wap_service_error(request):
+    logs = WapWebError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/waperror.html',context)
+
+def wx_service_error(request):
+    logs = WxServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/wxerror.html',context)
+
+def user_service_error(request):
+    logs = UserServiceError.objects.all()
+    logs = reversed(logs)
+    context = {'logs':logs}
+    return render(request,'log_analyze/usererror.html',context)
+
